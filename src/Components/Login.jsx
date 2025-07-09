00425345a9b100f8b36a8033879a80246a068db8
Surya Kumar <suryakumar531rsk@gmail.com>
2025-07-09 10:10:55 +0530

    Added action for form and handled signup and login in separate functions

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,34 +5,47 @@ import nameIcon from "../assets/person.png"
 
 export const Login = () => {
 
-  const [action,setAction] = useState("Sign up")
+  const [formtype,setFormtype] = useState("Sign up")
+
+  function signUp(formData){
+    const username = formData.get("username")
+    const email = formData.get("email")
+    const password = formData.get("password")
+    console.log(username)
+  }
+
+  function login(formData){
+    const email = formData.get("email")
+    const password = formData.get("password")
+    console.log(email)
+  }
 
   return (
     <>
-      <form className='input-container'>
+      <form className='input-container' action={formtype==="Sign up"?signUp:login}>
         <div className="input-header">
-          <div className={action==="Sign up"?"input-heading lightgray":"input-heading"} onClick={()=>setAction("Sign up")} >Sign up</div>
-          <div className={action==="Login"?"input-heading lightgray":"input-heading"} onClick={()=>setAction("Login")}>Login</div>
+          <div className={formtype==="Sign up"?"input-heading lightgray":"input-heading"} onClick={()=>setFormtype("Sign up")} >Sign up</div>
+          <div className={formtype==="Login"?"input-heading lightgray":"input-heading"} onClick={()=>setFormtype("Login")}>Login</div>
         </div>
         <div className='input-area'> 
-          {action==="Sign up"&&
+          {formtype==="Sign up"&&
           <div className="input">
             <img src={nameIcon} alt="username"/>
-            <input type="text" aria-label="username" placeholder='Username'></input>
+            <input type="text" aria-label="username" name='username' placeholder='Username'></input>
           </div>}
           <div className="input">
             <img src={emailIcon} alt="email"/>
-            <input type="email" aria-label="email" placeholder='E-mail'></input>
+            <input type="email" aria-label="email" name='email' placeholder='E-mail'></input>
           </div>
           <div className="input">
             <img src={passwordIcon} alt="password"/>
-            <input type="password" aria-label="password" placeholder='Password'></input>
+            <input type="password" aria-label="password" name='password' placeholder='Password'></input>
           </div>
         </div>
-        {action==="Login"&&
+        {formtype==="Login"&&
         <div className="forget-password">Forget Password? <span>click here</span></div>}
         <div className="submit-area">
-          <button className='submit'>{action}</button>
+          <button className='submit'>{formtype}</button>
         </div>
       </form>
     </>
